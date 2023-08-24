278b6529dd0a5694c483589766026eeeacdca5b2
mark <jdmaykl7@gmail.com>
2023-08-25 00:32:09 +0300

    Mod4_Ex1

---
+++ Module4_Exercise1/Module4_Exercise1/ReqresApiExample/Models/CreateUserParametersRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Module4_Exercise1.ReqresApiExample.Models;
+
+internal sealed class CreateUserParametersRequest
+{
+    public string Name { get; set; }
+
+    public string Job { get; set; }
+
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ Module4_Exercise1/Module4_Exercise1/ReqresApiExample/Models/ReqresUserRequest.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Module4_Exercise1.ReqresApiExample.Models;
+
+internal sealed class ReqresUserRequest
+{
+    public int Id { get; set; }
+
+    public string Email { get; set; }
+
+    [JsonProperty("first_name")]
+    public string FirstName { get; set; }
+
+    [JsonProperty("last_name")]
+    public string LastName { get; set; }
+
+    public string Avatar { get; set; }
+}
